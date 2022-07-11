c784874582258b379cf529dd35a20632e44d71de
Shiv Kumar Prasad <skprasad@ciena.com>
2022-07-11 08:38:24 +0000

    s3

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -13,3 +13,17 @@ resource "aws_s3_bucket_public_access_block" "archmydesaccess" {
   block_public_acls   = true
   block_public_policy = true
 }
+resource "aws_kms_key" "archmydeskms" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "archmydesencrypt" {
+  bucket = aws_s3_bucket.archmydes.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.archmydeskms.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
--- aws/modules/s3/versions.tf
+++ aws/modules/s3/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 0.13.0"
   required_providers {
-    aws      = "~> 3.70.0"
+    aws      = "~> 4.21.0"
     template = "~> 2.0"
     null     = "~> 2.0"
     local    = "~> 1.3"
